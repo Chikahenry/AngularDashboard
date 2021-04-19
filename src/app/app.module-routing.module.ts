9a03f5717b4021ba1154f913203271fb6b290a7b
Cika Henry <chikaejiofor95@yahoo.com>
2021-04-19 09:08:35 +0100

    ok

--- src/app/app.module-routing.module.ts
+++ src/app/app.module-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   children: [
 
   ] },
-  { path: '', component: AuthComponent },
+  { path: '/', component: AuthComponent },
 
   // { path: '**', component: PagenotfoundComponent },
 ];
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router'; 
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +11,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup; 
+  submitted = false;  
+  requestObj: any;
+  status: any;
+  emailAddress: any;
+  password: any;
+  fieldTextType: boolean;  
 
-  ngOnInit() {
+  // tslint:disable-next-line: max-line-length
+  constructor(private formBuilder: FormBuilder,
+    private http: HttpClient, 
+    private router: Router,  ) { }
+
+    ngOnInit(): void {
+     
+    this.loginForm = this.formBuilder.group({
+      emailAddress: ['', [Validators.required, Validators.maxLength(45)]],
+      password: ['', Validators.required], 
+    });
+ 
+
   }
 
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
+
+  onSubmit(formAllData: any) {
+    this.submitted = true;
+    
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.emailAddress = formAllData.emailAddress;
+    this.password = formAllData.password; 
+
+    let verifyRequestObj = {
+      email: this.emailAddress,
+      password: this.password, 
+    };
+ 
+
+    if (this.loginForm.valid) {
+      this.submitted = false;
+      let date = new Date();
+        localStorage.setItem('admin_email', this.emailAddress);
+        localStorage.setItem('admin_id', 1); 
+
+        localStorage.setItem('admin_role_id', 1); 
+        localStorage.setItem('admin_created_at', date);
+
+       Swal.fire({
+            icon: 'success',
+            title: 'Logged in',
+            text: "You're logged in successfully",
+            showConfirmButton: true,
+            timer: 5000
+        });
+        this.router.navigate(['/dashboard']);
+ 
+  }
 }
+   
+ 
+}
+
